41bd526537fb5e4f304cbfde7a353af964c1ac11
Ozkan <ozkayret@gmail.com>
2021-07-22 21:56:15 +0300

    null sefety

--- pin_entry_text_field/lib/pin_entry_text_field.dart
+++ pin_entry_text_field/lib/pin_entry_text_field.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class PinEntryTextField extends StatefulWidget {
-  final String lastPin;
+  final String? lastPin;
   final int fields;
-  final ValueChanged<String> onSubmit;
+  final ValueChanged<String>? onSubmit;
   final num fieldWidth;
   final num fontSize;
   final bool isTextObscure;
@@ -29,23 +29,23 @@ class PinEntryTextField extends StatefulWidget {
 }
 
 class PinEntryTextFieldState extends State<PinEntryTextField> {
-  List<String> _pin;
-  List<FocusNode> _focusNodes;
-  List<TextEditingController> _textControllers;
+  late List<String?> _pin;
+  late List<FocusNode?> _focusNodes;
+  late List<TextEditingController?> _textControllers;
 
   Widget textfields = Container();
 
   @override
   void initState() {
     super.initState();
-    _pin = List<String>(widget.fields);
-    _focusNodes = List<FocusNode>(widget.fields);
-    _textControllers = List<TextEditingController>(widget.fields);
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+     _pin = List.filled(widget.fields,null);
+    _focusNodes = List.filled(widget.fields,null);
+    _textControllers = List.filled(widget.fields,null);
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
       setState(() {
         if (widget.lastPin != null) {
-          for (var i = 0; i < widget.lastPin.length; i++) {
-            _pin[i] = widget.lastPin[i];
+          for (var i = 0; i < widget.lastPin!.length; i++) {
+            _pin[i] = widget.lastPin![i];
           }
         }
         textfields = generateTextFields(context);
@@ -55,7 +55,7 @@ class PinEntryTextFieldState extends State<PinEntryTextField> {
 
   @override
   void dispose() {
-    _textControllers.forEach((TextEditingController t) => t.dispose());
+    _textControllers.forEach((TextEditingController? t) => t!.dispose());
     super.dispose();
   }
 
@@ -76,7 +76,7 @@ class PinEntryTextFieldState extends State<PinEntryTextField> {
 
   void clearTextFields() {
     _textControllers.forEach(
-        (TextEditingController tEditController) => tEditController.clear());
+        (TextEditingController? tEditController) => tEditController!.clear());
     _pin.clear();
   }
 
@@ -87,18 +87,18 @@ class PinEntryTextFieldState extends State<PinEntryTextField> {
     if (_textControllers[i] == null) {
       _textControllers[i] = TextEditingController();
       if (widget.lastPin != null) {
-        _textControllers[i].text = widget.lastPin[i];
+        _textControllers[i]!.text = widget.lastPin![i];
       }
     }
 
-    _focusNodes[i].addListener(() {
-      if (_focusNodes[i].hasFocus) {}
+    _focusNodes[i]!.addListener(() {
+      if (_focusNodes[i]!.hasFocus) {}
     });
 
-    final String lastDigit = _textControllers[i].text;
+    final String lastDigit = _textControllers[i]!.text;
 
     return Container(
-      width: widget.fieldWidth,
+      width: widget.fieldWidth as double?,
       margin: EdgeInsets.only(right: 10.0),
       child: TextField(
         controller: _textControllers[i],
@@ -109,7 +109,7 @@ class PinEntryTextFieldState extends State<PinEntryTextField> {
         style: TextStyle(
             fontWeight: FontWeight.bold,
             // color: Colors.black,
-            fontSize: widget.fontSize),
+            fontSize: widget.fontSize as double?),
         focusNode: _focusNodes[i],
         obscureText: widget.isTextObscure,
         decoration: InputDecoration(
@@ -122,25 +122,25 @@ class PinEntryTextFieldState extends State<PinEntryTextField> {
             _pin[i] = str;
           });
           if (i + 1 != widget.fields) {
-            _focusNodes[i].unfocus();
+            _focusNodes[i]!.unfocus();
             if (lastDigit != null && _pin[i] == '') {
               FocusScope.of(context).requestFocus(_focusNodes[i - 1]);
             } else {
               FocusScope.of(context).requestFocus(_focusNodes[i + 1]);
             }
           } else {
-            _focusNodes[i].unfocus();
+            _focusNodes[i]!.unfocus();
             if (lastDigit != null && _pin[i] == '') {
               FocusScope.of(context).requestFocus(_focusNodes[i - 1]);
             }
           }
-          if (_pin.every((String digit) => digit != null && digit != '')) {
-            widget.onSubmit(_pin.join());
+          if (_pin.every((String? digit) => digit != null && digit != '')) {
+            widget.onSubmit!(_pin.join());
           }
         },
         onSubmitted: (String str) {
-          if (_pin.every((String digit) => digit != null && digit != '')) {
-            widget.onSubmit(_pin.join());
+          if (_pin.every((String? digit) => digit != null && digit != '')) {
+            widget.onSubmit!(_pin.join());
           }
         },
       ),
